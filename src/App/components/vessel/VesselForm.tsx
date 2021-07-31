17c6a7a3d02c1197d264e46a4b0fb61cdad06f6f
Choor Hong <abner.hong@hotmail.com>
2021-07-31 14:43:33 +0800

    Edit vessel module

--- src/App/components/vessel/VesselForm.tsx
+++ src/App/components/vessel/VesselForm.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Form, Input, Button, DatePicker } from 'antd'
 
 import { IVesselFormProps, SubmitValues } from '../types/vessel'
 import axios from 'axios'
+import moment from 'moment'
 
 const { REACT_APP_BASE_URL: baseUrl } = process.env
 
@@ -37,19 +38,25 @@ const tailLayout = {
   }
 }
 
-const VesselForm: React.FC<IVesselFormProps> = ({ initialValues, onSave }) => {
+const VesselForm: React.FC<IVesselFormProps> = ({ initialValues, onSave, disabled }) => {
   const [form] = Form.useForm()
 
   // TODO: Handle submit for both create & edit
   const handleSubmit = onSave || (async (values: SubmitValues) => {
-    const val = {
+    let val = {
       ...values,
       name: values.name.trim().toUpperCase()
     }
-    console.log('val', val)
+    let url = `${baseUrl}/vessel/create`
+
+    if (initialValues) { // for edit vessel
+      url = `${baseUrl}/vessel/update`
+      val = { ...val, id: initialValues.id }
+    }
+
     try {
-      const result = await axios.post(`${baseUrl}/vessel/create`, val)
-      if (result && result.status === 201 && result.data) {
+      const result = await axios.post(url, val)
+      if (result && result.data) {
         console.log('result', result)
         // history.push(`/${baseUrl}/purchase-order/${result.data.id}`)
       }
@@ -58,12 +65,23 @@ const VesselForm: React.FC<IVesselFormProps> = ({ initialValues, onSave }) => {
     }
   })
 
+  const defaultEditValues = useMemo(() => {
+    if (!initialValues) return
+    const { name, earliestReturningDate, cutOff, remarks } = initialValues
+    return {
+      name,
+      earliestReturningDate: moment(earliestReturningDate),
+      cutOff: moment(cutOff),
+      remarks
+    }
+  }, [initialValues])
+
   return (
     <div style={{ padding: '2% 2%' }}>
       <Form
         {...layout}
         form={form}
-        initialValues={initialValues}
+        initialValues={defaultEditValues}
         name='vessel-form'
         onFinish={handleSubmit}
       >
@@ -77,7 +95,7 @@ const VesselForm: React.FC<IVesselFormProps> = ({ initialValues, onSave }) => {
             }
           ]}
         >
-          <Input />
+          <Input disabled={disabled} />
         </Form.Item>
 
         <Form.Item
@@ -90,7 +108,7 @@ const VesselForm: React.FC<IVesselFormProps> = ({ initialValues, onSave }) => {
             }
           ]}
         >
-          <DatePicker style={{ width: '100%' }} />
+          <DatePicker style={{ width: '100%' }} disabled={disabled} />
         </Form.Item>
 
         <Form.Item
@@ -103,23 +121,23 @@ const VesselForm: React.FC<IVesselFormProps> = ({ initialValues, onSave }) => {
             }
           ]}
         >
-          <DatePicker format='YYYY-MM-DD HH:mm' showTime={{ format: 'HH:mm' }} style={{ width: '100%' }} />
+          <DatePicker format='YYYY-MM-DD HH:mm' showTime={{ format: 'HH:mm' }} style={{ width: '100%' }} disabled={disabled} />
         </Form.Item>
 
         <Form.Item
           name='remarks'
           label='Note/Remarks'
         >
-          <Input.TextArea rows={4} />
+          <Input.TextArea rows={4} disabled={disabled} />
         </Form.Item>
 
-        {!initialValues && (
-          <Form.Item {...tailLayout}>
-            <Button type='primary' htmlType='submit'>
-              Submit
-            </Button>
-          </Form.Item>
-        )}
+        {/* {!initialValues && ( */}
+        <Form.Item {...tailLayout}>
+          <Button type='primary' htmlType='submit' disabled={disabled}>
+            Submit
+          </Button>
+        </Form.Item>
+        {/* )} */}
       </Form>
     </div>
   )
