0a80edafdb76ddd575968d070d5a78f8d5c9d2c8
Yang Wei Hao <whyang0808@gmail.com>
2021-09-13 14:50:49 +0800

    Add advanced filter to vessel page

---
+++ src/App/components/vessel/FilterButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { DatePicker, Form } from 'antd'
+
+import FilterButtonBuilder from '../_shared/FilterButtonBuilder'
+import { FilterButtonProps } from '../types/filterButton'
+
+const formItems = (
+  <>
+    <Form.Item
+      name='cutOffStartDate'
+      label='Cut Off Start Date'
+    >
+      <DatePicker format='YYYY-MM-DD HH:mm' showTime={{ format: 'HH:mm' }} style={{ width: '100%' }} />
+    </Form.Item>
+
+    <Form.Item
+      name='cutOffEndDate'
+      label='Cut Off End Date'
+    >
+      <DatePicker format='YYYY-MM-DD HH:mm' showTime={{ format: 'HH:mm' }} style={{ width: '100%' }} />
+    </Form.Item>
+  </>
+)
+
+const FilterButton: React.FC<FilterButtonProps> = ({ onSave }) => (
+  <FilterButtonBuilder
+    cardStyle={{ width: 600 }}
+    formItems={formItems}
+    onSave={onSave}
+  />
+)
+
+export default FilterButton
--- src/App/pages/vessel/vessel.tsx
+++ src/App/pages/vessel/vessel.tsx
@@ -1,34 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
 import Nav from '../../layout/Nav'
 import SearchBar from '../../components/_shared/SearchBar'
-import { VesselList } from '../../components/vessel'
+import { VesselList, FilterButton } from '../../components/vessel'
 import axiosAuth from '../../axios'
+import { useQuery } from '../../hooks/query-hook'
 
 const VesselPage: React.FC = (props) => {
+  const history = useHistory()
+  const { search, searchQuery, stringify } = useQuery()
   const [data, setData] = useState<any[]>([])
 
   useEffect(() => {
     (async () => {
+      let url = '/vessel'
+
+      if (search) {
+        url = `/vessel${search}`
+      }
+
       try {
-        const vessels = await axiosAuth.get('/vessel')
-        console.log('vessels', vessels)
-        if (vessels && vessels.data) {
-          setData(vessels.data)
+        const { data } = await axiosAuth.get(url)
+        if (data) {
+          setData(data)
         }
       } catch (error) {
         console.log('error', error)
       }
     }
     )()
-  }, [])
+  }, [search])
+
+  const handleFilterSave = useCallback((values: Record<string, string>) => history.push(`?${stringify(values)}`), [history, stringify])
 
   return (
     <Nav>
       <SearchBar
-        type='vessel'
+        advanceFilter={<FilterButton onSave={handleFilterSave} />}
         SearchProps={{
+          defaultValue: searchQuery.name as string ?? '',
+          onSearch: (value: string) => history.push(`?name=${value}`),
           placeholder: 'Search by Vessel'
         }}
+        type='vessel'
       />
       <VesselList data={data} />
     </Nav>
