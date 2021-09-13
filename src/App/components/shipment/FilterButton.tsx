3a5b0f4f29efe8ac7ef6af1b6e5f5c7d2412e44b
Yang Wei Hao <whyang0808@gmail.com>
2021-09-14 01:56:30 +0800

    Add advanced filter to shipment page

---
+++ src/App/components/shipment/FilterButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Form, Select } from 'antd'
+
+import InputSearch from '../_shared/InputSearch'
+import FilterButtonBuilder from '../_shared/FilterButtonBuilder'
+import { FilterButtonProps } from '../types/filterButton'
+import { STATUS_OPTIONS } from './constants'
+
+const { Option } = Select
+
+const formItems = (
+  <>
+    <Form.Item name='vendor' label='Vendor'>
+      <InputSearch isContact placeholder='Search Vendor' />
+    </Form.Item>
+    <Form.Item name='bookingId' label='Booking'>
+      <InputSearch isBooking placeholder='Search Booking' />
+    </Form.Item>
+    <Form.Item name='status' label='Status'>
+      <Select allowClear placeholder='Select Status'>
+        {STATUS_OPTIONS.map(option => (<Option key={option.value} value={option.value}>{option.label}</Option>))}
+      </Select>
+    </Form.Item>
+  </>
+)
+
+const FilterButton: React.FC<FilterButtonProps> = ({ onSave }) => (
+  <FilterButtonBuilder
+    formItems={formItems}
+    onSave={onSave}
+  />
+)
+
+export default FilterButton
--- src/App/components/shipment/index.tsx
+++ src/App/components/shipment/index.tsx
@@ -1,2 +1,3 @@
+export { default as FilterButton } from './FilterButton'
 export { default as ShipmentForm } from './ShipmentForm'
 export { default as ShipmentList } from './ShipmentList'
