68b535a39a2797663dbc540fab4dd976700ff951
Yang Wei Hao <whyang0808@gmail.com>
2021-09-17 19:38:03 +0800

    Populate advanced filter in booking page from URL

--- src/App/pages/booking/booking.tsx
+++ src/App/pages/booking/booking.tsx
@@ -1,16 +1,22 @@
-import React, { useEffect, useCallback, useState } from 'react'
+import React, { useEffect, useCallback, useState, useMemo } from 'react'
 import { useHistory } from 'react-router-dom'
+import moment from 'moment'
 
 import Nav from '../../layout/Nav'
 import { BookingList, FilterButton } from '../../components/booking'
 import SearchBar from '../../components/_shared/SearchBar'
 import axiosAuth from '../../axios'
 import { useQuery } from '../../hooks/query-hook'
+import { weekEnd, weekStart } from '../../utils/dates'
 
 const BookingPage: React.FC = (props) => {
   const history = useHistory()
   const { search, searchQuery, stringify } = useQuery()
   const [data, setData] = useState<any[]>([])
+
+  const initialValues = useMemo(() => searchQuery.cutOffStartDate && searchQuery.cutOffEndDate
+    ? { ...searchQuery, cutOffStartDate: moment(searchQuery.cutOffStartDate), cutOffEndDate: moment(searchQuery.cutOffEndDate) }
+    : { ...searchQuery, cutOffStartDate: weekStart, cutOffEndDate: weekEnd }, [searchQuery])
 
   useEffect(() => {
     (async () => {
@@ -38,7 +44,7 @@ const BookingPage: React.FC = (props) => {
   return (
     <Nav>
       <SearchBar
-        advanceFilter={<FilterButton onSave={handleFilterSave} />}
+        advanceFilter={<FilterButton initialValues={initialValues} onSave={handleFilterSave} />}
         SearchProps={{
           defaultValue: searchQuery.bookingId as string ?? '',
           onSearch: (value: string) => history.push(`?bookingId=${value}`),
